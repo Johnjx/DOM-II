2318cea4cbdbadb01c71a93231b91b0394049186
John Ruddock <john.jxr@pm.me>
2022-04-12 15:26:17 -0400

    added mouseover and mouseleave event listeners to h1 element

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 import './less/index.less'
 
 // Your code goes here!
+// 10 event listeners
+
+// mouseover and mouseleave
+
+const h1 = document.querySelector("header div h1");
+
+h1.addEventListener("mouseover", evt => {
+    evt.target.style.color = 'Blue';
+});
+
+h1.addEventListener("mouseleave", evt => {
+    evt.target.style.color = "gold"
+});
+
+
+
